77fde38782b42e08f6510dbe2b51022105cc72dd
eris6 <erispatel20@gmail.com>
2024-10-05 18:08:20 -0400

    Add rainbow color and darkening functionality
    Etch a sketch box now will have a random RGB value
    and will darken with each interaction, becoming completely
    dark after 10 hovers

--- etchasketch.js
+++ etchasketch.js
@@ -1,19 +1,22 @@
 const container = document.querySelector(".container");
 const resize = document.querySelector(".resize");
+let dimension = 16;
+
 resize.addEventListener('click', ()=>{
     let newSize = prompt("What size?");
     while(container.firstChild){
         container.removeChild(container.lastChild);
     }
-    if (newSize > 100){
-        createGrid(100);
+    if (newSize < 100){
+        createGrid(newSize);
+        dimension = newSize;
     }
     else{
-        
-        createGrid(newSize);
+        createGrid(100);
+        dimension = 100;
+
     }
 })
-
 
 
 function createGrid(dimension){
@@ -27,21 +30,82 @@ function createGrid(dimension){
             const box = document.createElement("div");
             box.style.height=boxSize + "px";
             box.style.width=boxSize + "px";
+            box.style.background="#F5A9B8";
+            
+
             row.appendChild(box);
             
             box.classList.add("box");
             box.addEventListener('mouseover', (event)=>{
-                box.style.background="#5BCEFA"
+                if (box.style.background == "rgb(245, 169, 184)"){
+                    box.style.background= getRandomColor();
+                    box.style.filter = "brightness(100%)";
+                    
+                }
+                else{
+                    switch(box.style.filter){
+                        case "brightness(100%)":
+                            box.style.filter = "brightness(90%)";
+                            break;
+                        case "brightness(90%)":
+                            box.style.filter = "brightness(80%)";
+                            break; 
+                        case "brightness(80%)":
+                            box.style.filter = "brightness(70%)";
+                            break;  
+                        case "brightness(70%)":
+                            box.style.filter = "brightness(60%)";
+                            break;  
+                        case "brightness(60%)":
+                            box.style.filter = "brightness(50%)";
+                            break;  
+                        case "brightness(50%)":
+                            box.style.filter = "brightness(40%)";
+                            break;  
+                        case "brightness(40%)":
+                            box.style.filter = "brightness(30%)";
+                            break;  
+                        case "brightness(30%)":
+                            box.style.filter = "brightness(20%)";
+                            break; 
+                        case "brightness(20%)":
+                            box.style.filter = "brightness(10%)";
+                            break;  
+                        case "brightness(10%)":
+                            box.style.filter = "brightness(0%)";
+                            break;                      
+                    }
+                }
             })
         }
     }
 }
+
+function getRandomColor(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let randomColor = "rgba" + "(" + red + " " + green + " " + blue + ")"
+    return randomColor;
+}
+
+function opacify(rgb){
+    let value = rgb(10, rgb.length);
+    console.log(value);
+    
+    rgb = rgb.slice(0, 10) + " / 10%" + ")";
+    // console.log(rgb);
+    return rgb;
+
+
+}
+
 
 function getBoxSize(dimension){
     return 800/dimension - 2;
 }
 
 
-createGrid(15);
+createGrid(dimension);
 
 
