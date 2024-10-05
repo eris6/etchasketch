1f322eb300d35af87cde71462b3d6a56ccb22fef
eris6 <erispatel20@gmail.com>
2024-10-05 10:31:09 -0400

    Grid now has resizing properties!
    Grid can now be resized based on the inputted
    square dimension. Just don't put anything over a 100
    because that screwed with the browser's resources.
    But anything from 0x0-100x100 will work now!

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,30 @@
-const container = document.querySelector(".container");
-for (let i =0; i < 16; i++){
-    const row = document.createElement("div");
-    container.appendChild(row);
+function createGrid(dimension){
+    boxSize = getBoxSize(dimension);
+    const container = document.querySelector(".container");
+    for (let i =0; i < dimension; i++){
+        const row = document.createElement("div");
+        container.appendChild(row);
 
-    for (let j = 0; j < 16; j++){
-        const box = document.createElement("div");
-        row.appendChild(box);
-        box.classList.add("box");
-        box.addEventListener('mouseover', (event)=>{
-            box.setAttribute("style", "background-color:#5BCEFA")
-        })
+        for (let j = 0; j < dimension; j++){
+            const box = document.createElement("div");
+            box.style.height=boxSize + "px";
+            box.style.width=boxSize + "px";
+            box.style.padding="0";
+            row.appendChild(box);
+            
+            box.classList.add("box");
+            box.addEventListener('mouseover', (event)=>{
+                box.style.background="#5BCEFA"
+            })
+        }
     }
 }
+
+function getBoxSize(dimension){
+    return 800/dimension - 2;
+}
+
+
+createGrid(16);
+
 
